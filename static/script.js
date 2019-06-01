8b4de9d452dae448a07056287caeea377b98e2ae
LeoKnox <LeoKnox@users.noreply.github.com>
2019-05-31 20:46:03 -0700

    Add files via upload
    Added code updating css position and transform.

--- static/script.js
+++ static/script.js
@@ -16,3 +16,9 @@ function addDoor() {
         <input type='number' name=height${doorNum}>
         <br><br>`;
 }
+
+function update() {
+    var roomWidth = document.getElementsByClassName("box__face--back");
+    roomWidth[0].style.transform = "translateZ(150px)";
+    roomWidth[0].style.width = "600";
+}
