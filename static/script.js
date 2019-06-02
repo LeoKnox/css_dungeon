c7895113370bcf32c0604d7e3e0cc21e8aa58c39
LeoKnox <LeoKnox@users.noreply.github.com>
2019-06-01 20:23:08 -0700

    Add files via upload
    Figuring out math for updating grid.

--- static/script.js
+++ static/script.js
@@ -18,7 +18,20 @@ function addDoor() {
 }
 
 function update() {
-    var roomWidth = document.getElementsByClassName("box__face--back");
-    roomWidth[0].style.transform = "translateZ(150px)";
-    roomWidth[0].style.width = "600";
+    var x = 600;
+    var y = 600;
+    var roomLeft =document.getElementsByClassName("box__face--left");
+    var roomRight = document.getElementsByClassName("box__face--right");
+    var roomFloor = document.getElementsByClassName("box__face--floor");
+    var roomBack = document.getElementsByClassName("box__face--back");
+    //roomWidth[0].style.transform = "translateZ(150px)";
+    roomFloor[0].style.width = y;
+    roomFloor[0].style.height = x;
+    roomFloor[0].style.transform = `translateZ(${y/2-x/2}px) rotateX(-90deg)`
+    roomBack[0].style.width = y;
+    roomBack[0].style.transform = `translateZ(${-y/2}px)`;
+    roomRight[0].style.width = x;
+    roomRight[0].style.transform = `translateZ(${y-x}px) translateX(${y/2+50}px) rotateY(90deg)`;
+    roomLeft[0].style.width = y;
+    roomLeft[0].style.transform =`translateZ(${y-x}px) translateX(${-y/2+50}px) rotateY(-90deg)`;
 }
